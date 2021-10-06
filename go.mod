397a44ae289e9fe1fac0b9ae4133259c4a89c044
Ivan <IvanBychkov@mail.ru>
2021-10-06 19:52:41 +0300

    vendoring

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/ivanbychkov27/go-heroku
 
 go 1.17
-
-require github.com/heroku/x v0.0.32
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	_ "github.com/heroku/x/hmetrics/onload"
 	"net"
 	"net/http"
 	"os"
