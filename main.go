759d33426acf51083682b57ef18f6c63c18f8414
Ivan <IvanBychkov@mail.ru>
2021-10-07 13:28:08 +0300

    коррект

--- main.go
+++ main.go
@@ -1,89 +1,50 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"net"
-	"net/http"
+	"io/ioutil"
 	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-type Application struct {
-	server *http.Server
-}
-
-func New() *Application {
-	app := &Application{}
-
-	router := http.NewServeMux()
-	router.HandleFunc("/", handler)
-
-	app.server = &http.Server{}
-	app.server.Handler = router
-
-	return app
-}
+const doc = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Go-heroku</title>
+`
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
-		port = "4000"
-		//log.Fatal("$PORT must be set")
+		fmt.Println("$PORT must be set")
+		return
 	}
 
-	addr := "0.0.0.0:" + port
-
-	ln, err := net.Listen("tcp", addr)
-	if err != nil {
-		fmt.Println("error create go-heroku listener: ", err)
-	}
-	defer ln.Close()
-
-	app := New()
-
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	go app.run(cancel, wg, ln)
-
-	<-ctx.Done()
-
-	app.stopServer()
-	wg.Wait()
-
-	fmt.Println("Done...")
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.GET("/", mainHandler)
+	router.Run(":" + port)
 }
 
-func (app *Application) run(cancel context.CancelFunc, wg *sync.WaitGroup, ln net.Listener) {
-	defer wg.Done()
-	defer cancel()
-
-	fmt.Println("start server listen address", ln.Addr().String())
-
-	err := app.server.Serve(ln)
-	if err != nil && err.Error() != "http: Server closed" {
-		fmt.Println("error serve ", err.Error())
-	}
-
+func mainHandler(c *gin.Context) {
+	fileName := "index.tmpl.html"
+	saveFileHTML(fileName)
+	c.HTML(200, fileName, nil)
 }
 
-func (app *Application) stopServer() {
-	fmt.Println("stop server...")
-	err := app.server.Shutdown(context.Background())
-	if err != nil {
-		fmt.Println("error stop go-heroku server:", err.Error())
-	}
-}
-
-func handler(resp http.ResponseWriter, req *http.Request) {
+func saveFileHTML(fileName string) {
 	t := time.Now().Format("02:04:06 15:04:05")
-	res := "<html><body><H1> Time: " + t + " </H1></body></html>"
-	resp.Write([]byte(res))
+	res := doc + "<body><H1> Heroku time: " + t + " </H1></body></html>"
+
+	fileName = "templates/" + fileName
+	err := ioutil.WriteFile(fileName, []byte(res), 0777)
+	if err != nil {
+		fmt.Println("error write file ", fileName)
+	}
 }
